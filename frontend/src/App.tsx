33943c8ceaa0c24db0ee811fcbd9feadace0b59d
Enzo Sanchez <enzojsanchez98@gmail.com>
2025-04-03 10:14:25 -0300

    chore: frontend set-up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+		<div className="min-h-screen flex items-center justify-center ">
+			<h1 className='text-5xl'>Hello world</h1>
+		</div>
+  )
+}
+
+export default App
